fb2460ed75f0e86b9df3264f6e521fc534f3a0f2
duytd34 <duytdbh01244@fpt.edu.vn>
2025-03-27 09:34:22 +0700

    update Models

--- ASM_APDP_nhom6/Models/Course.cs
+++ ASM_APDP_nhom6/Models/Course.cs
@@ -1,6 +1,24 @@
-﻿namespace ASM_APDP_nhom6.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace ASM_APDP_nhom6.Models
 {
-	public class Course
-	{
-	}
+    public class Course
+    {
+        [Key]
+        public int Id { get; set; } // PK
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; } 
+
+        [Required]
+        [StringLength(500)]
+        public string Description { get; set; } 
+        [Required]
+        [Range(1, 10)]
+        public int Credits { get; set; }
+
+        public List<Grade> Grades { get; set; }
+    }
 }
--- ASM_APDP_nhom6/Models/Grade.cs
+++ ASM_APDP_nhom6/Models/Grade.cs
@@ -1,6 +1,43 @@
-﻿namespace ASM_APDP_nhom6.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ASM_APDP_nhom6.Models
 {
-	public class Grade
-	{
-	}
+    public class Grade
+    {
+        [Key]
+        public int Id { get; set; } 
+
+        [Required]
+        public int UserId { get; set; } 
+
+        [ForeignKey("UserId")]
+        public User User { get; set; } 
+
+        [Required]
+        public int CourseId { get; set; } 
+
+        [ForeignKey("CourseId")]
+        public Course Course { get; set; } 
+
+        [Required]
+        [Range(0, 100)]
+        public double Score { get; set; } 
+
+        public string LetterGrade { get; set; } 
+
+        public void CalculateLetterGrade()
+        {
+            if (Score >= 90)
+                LetterGrade = "A";
+            else if (Score >= 80)
+                LetterGrade = "B";
+            else if (Score >= 70)
+                LetterGrade = "C";
+            else if (Score >= 60)
+                LetterGrade = "D";
+            else
+                LetterGrade = "F";
+        }
+    }
 }
